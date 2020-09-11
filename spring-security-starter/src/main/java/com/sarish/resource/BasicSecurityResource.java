653f73068d18e27f5e078266fda25f3ee374e8ef
sarishbiswas <sarishbiswas@gmail.com>
2020-09-12 00:52:17 +0530

    Basic Authorization with Admin and User roles and Authorize with httpSecurity

--- spring-security-starter/src/main/java/com/sarish/resource/BasicSecurityResource.java
+++ spring-security-starter/src/main/java/com/sarish/resource/BasicSecurityResource.java
@@ -1,12 +1,24 @@
 package com.sarish.resource;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BasicSecurityResource {
-	@RequestMapping("/welcome")
+	//Accessible by Everyone
+	@GetMapping("/")
 	public String getWelcome() {
 		return "<h1>WELCOME</h1>";
+	}
+	
+	//Accessible By only Logged In User . here Admin and User.
+	@GetMapping("/user")
+	public String user() {
+		return "<h1>WELCOME USER</h1>";
+	}
+	//Only Accessible by Admin
+	@GetMapping("/admin")
+	public String admin() {
+		return "<h1>WELCOME Admin</h1>";
 	}
 }
